054155ba072bc40ca1c503b0156a47233640a020
João Vitor <joaossc.ege@gmail.com>
2025-01-19 12:13:58 -0300

    Estrutura báisca e leitura da usart

--- controle-pinos-gpio-bitdoglab.c
+++ controle-pinos-gpio-bitdoglab.c
@@ -1,7 +1,63 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-#define PINO_BUZZER 15
+#define LED_VERMELHO 13
+#define LED_AZUL 12
+#define LED_VERDE 11
+#define PINO_BUZZER 21
+
+
+
+char entrada;
+
+//Funções 
+void iniciar_pinos(); 
+void ativar_buzzer(int duration);
+
+int main() {
+    iniciar_pinos(); 
+    stdio_init_all(); 
+    sleep_ms(500);   
+
+    while (true) {  
+        printf("Aguardando entrada: ");
+
+        // Usa scanf para capturar um caractere
+        if (scanf(" %c", &entrada) == 1) { // Captura e verifica se foi bem-sucedido
+            printf("\nEntrada: %c\n", entrada);
+          switch (entrada){
+            case 'A':
+            printf("LED VERMELHO ON \n");
+            break;
+            case 'B':
+            printf("BUZZER! \n");
+            ativar_buzzer(1000);
+            break;
+            case 'F':
+            printf("ENTRANDO NO MODO BOOT! \n");
+            break;
+            default:
+            printf("ENTRADA INVÁLIDA \n");
+          }
+        } else {
+            printf("\nErro ao capturar entrada.\n");
+        }
+
+        sleep_ms(250); // Aguarda um pouco antes de repetir
+    }
+}
+
+void iniciar_pinos(){
+    gpio_init(LED_VERMELHO);
+    gpio_init(LED_VERDE);
+    gpio_init(LED_AZUL);
+    gpio_init(PINO_BUZZER);
+
+    gpio_set_dir(LED_VERMELHO, GPIO_OUT);
+    gpio_set_dir(LED_VERDE, GPIO_OUT);
+    gpio_set_dir(LED_AZUL, GPIO_OUT);
+    gpio_set_dir(PINO_BUZZER, GPIO_OUT);
+}
 
 // Aciona o buzzer
 void ativar_buzzer(int duration)
@@ -16,14 +72,3 @@ void ativar_buzzer(int duration)
     }
 }
 
-int main()
-{
-    stdio_init_all();
-
-    while (true)
-    {
-        printf("Hello, world!\n");
-        ativar_buzzer(1000);
-        sleep_ms(1000);
-    }
-}
