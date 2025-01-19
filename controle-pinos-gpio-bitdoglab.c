a5706a754b514d65ad4678587fc03813bd763217
Erick Barros Ferreira Gomes <erickgomes.developer@gmail.com>
2025-01-18 23:21:02 -0300

    feature para ativação do buzzer

--- controle-pinos-gpio-bitdoglab.c
+++ controle-pinos-gpio-bitdoglab.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define PINO_BUZZER 15
 
+// Aciona o buzzer
+void ativar_buzzer(int duration)
+{
+    duration /= 2; // Divide a duração por 2 para gerar pulsos mais curtos
+    for (int i = 0; i < duration; i++)
+    {
+        gpio_put(PINO_BUZZER, 1);
+        sleep_us(500); // Meio período do ciclo
+        gpio_put(PINO_BUZZER, 0);
+        sleep_us(500); // Meio período do ciclo
+    }
+}
 
 int main()
 {
     stdio_init_all();
 
-    while (true) {
+    while (true)
+    {
         printf("Hello, world!\n");
+        ativar_buzzer(1000);
         sleep_ms(1000);
     }
 }
